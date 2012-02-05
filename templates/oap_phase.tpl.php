b1d1bf513c5cb029594908a816ee26c25dffb87a
Ryan Palmer <rypalmer@gmail.com>
2012-02-05 12:15:20 +0200

    Tidy up one class name

--- templates/oap_phase.tpl.php
+++ templates/oap_phase.tpl.php
@@ -1,5 +1,5 @@
 <div id="oa-phase-<?php print $id; ?>" class="oap-phase oap-phase-status-<?php print $status; ?>">
-  <div class="oap-phase-title">
+  <div class="oap-phase-title-status">
     <h3><?php print $title; ?></h3>
     <div class="oap-status-icon oap-status-<?php print $status; ?>"><?php print $status_tip; ?></div>
   </div>
