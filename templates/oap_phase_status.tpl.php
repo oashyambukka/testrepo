cd489057017c540d37d122f2bc526ff16208cf7b
Ryan Palmer <rypalmer@gmail.com>
2012-02-05 19:45:09 +0200

    Add Phase status block

---
+++ templates/oap_phase_status.tpl.php
@@ -0,0 +1,8 @@
+<div id="oa-phase-status-<?php print $id; ?>" class="oap-phase-status oap-phase-status-status-<?php print $status; ?>">
+  <div class="oap-phase-title">
+    <h3><?php print $title; ?></h3>
+  </div>
+  <div class="oa-phase-phases">
+    <?php print $phases; ?>
+  </div>
+</div>
