699e049fde7ce307814636419c381553c098802b
Ryan Palmer <rypalmer@gmail.com>
2012-02-25 18:42:31 -0400

    JS for paycheck form

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
-	$(".oap-tooltip-trigger").click(function(){
-		$(".oap-tooltip").css("display","block");
-	}, function(){
-		$(".oap-tooltip").css("display","none");
-	});
-	$(".oap-tooltip .close").click(function(){
-		$(".oap-tooltip").css("display","none");
-	});
-	$(".add-another-link").click(function() {
-		$(this).hide();
-		$(this).parent().next().slideDown();
-	});
+
+  $(".oap-tooltip-trigger").click(
+    function() {
+      $(".oap-tooltip").css("display","block");
+    },
+    function(){
+      $(".oap-tooltip").css("display","none");
+  });
+
+  $(".oap-tooltip .close").click(function() {
+      $(".oap-tooltip").css("display","none");
+  });
+
+  $(".add-another-link").click(function() {
+      $(this).hide();
+      $(this).parent().next().slideDown();
+  });
+
+
+  $('#edit-delivery-rdo-delivery-option-directdeposit-wrapper input').change(function () {
+    var value = $(this).val();
+    fieldset_hide('fieldset-directdeposit');
+    fieldset_hide('fieldset-cashpay');
+    if (value == 'cashpay' || value = 'directdeposit') {
+      fieldset_show('fieldset-' + value);
+    }
+  });
+  
+  function fieldset_hide($id) {
+    if (!$(id).hasClass('collapsed')) {
+      $('#fieldset-directdeposit legend a').click();
+    }
+  }
+  function fieldset_show($id) {
+    if ($(id).hasClass('collapsed')) {
+      $('#fieldset-directdeposit legend a').click();
+    }
+  }
+
 });
