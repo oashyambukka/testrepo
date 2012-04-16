2b8099e5e9eca6887826cbc489966548ddb4cf6d
Ryan Palmer <rypalmer@gmail.com>
2012-04-16 11:32:53 -0700

    Messages framework

---
+++ templates/oap_message_page.tpl.php
@@ -0,0 +1,8 @@
+<div class="oap-message-page">
+  <div class="full-message">
+    <div class="oap-message-date"><?php print $date; ?></div>
+    <div class="oap-message-subject"><?php print $subject ?></div>
+    <div class="oap-message-body"><?php print $body ?></div>
+    <div class="close-message"><a href="/messages">Close x</a></div>
+  </div>
+</div>
--- templates/oap_message_row.tpl.php
+++ templates/oap_message_row.tpl.php
@@ -1,5 +1,12 @@
 <div class="oap-message-row">
   <div class="oap-message-date"><?php print $date; ?></div>
-  <div class="oap-message-teaser"><?php print $teaser ?></div>
+  <div class="oap-message-subject"><?php print $subject ?></div>
   <div class="oap-message-link"><?php print $link; ?></div>
+  
+  <div class="full-message">
+    <div class="oap-message-date"><?php print $date; ?></div>
+    <div class="oap-message-subject"><?php print $subject ?></div>
+    <div class="oap-message-body"><?php print $body ?></div>
+    <div class="close-message"><span class="close-message-link">Close x</a></div>
+  </div>
 </div>
