f3593cb7beaf81476e3119c95d3aa37bfc1740d3
Ryan Palmer <rypalmer@gmail.com>
2012-05-09 14:23:53 -0300

    Tutorial page, tooltips

--- templates/oap_tooltip.tpl.php
+++ templates/oap_tooltip.tpl.php
@@ -2,5 +2,5 @@
 
 <div class="oap-tooltip oap-tip-<?php print $status; ?>">
   <div class="close"><a>CLOSE</a></div>
-  <div class="string"><?php print $string; ?></div>
+  <div class="string"><?php print $body; ?></div>
 </div>
