12f73a95fa0f0046ab1eb969a669f9c8b39ee513
Ryan Palmer <rypalmer@gmail.com>
2012-05-14 17:05:17 -0300

    Add CSS and JS for onboard process

---
+++ js/onboard.js
@@ -0,0 +1,28 @@
+Drupal.behaviors.onboard = function (context) {
+
+  // If email address field changes, check for dupes
+  $('#edit-Candidates-Email').change(function() {
+    var data = {};
+    data['email'] = $(this).val();
+    $.ajax({
+      url: '/apply/dupecheck',
+      type: "POST",
+      dataType: "json",
+      data: data,
+      complete: function(XMLHttpRequest, textStatus) {
+//console.log(XMLHttpRequest);
+      },
+      success: function(data) {
+        if (data.status == 'dupe') {
+          $('#email-address-is-dupe').slideDown();
+          $('#edit-submit').hide();
+        }
+        else if (data.status == 'no-dupe') {
+          $('#email-address-is-dupe').slideUp();
+          $('#edit-submit').show();
+        }
+      }
+    });
+  });
+
+}
