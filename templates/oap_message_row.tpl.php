ce0398c49386db6e45ebcf329780d741580b750b
Ryan Palmer <rypalmer@gmail.com>
2012-05-16 09:45:21 -0300

    Messages -- operational

--- templates/oap_message_row.tpl.php
+++ templates/oap_message_row.tpl.php
@@ -3,7 +3,7 @@
   <div class="oap-message-subject"><?php print $subject ?></div>
   <div class="oap-message-link"><?php print $link; ?></div>
   
-  <div class="full-message">
+  <div class="full-message set-hide">
     <div class="oap-message-date"><?php print $date; ?></div>
     <div class="oap-message-subject"><?php print $subject ?></div>
     <div class="oap-message-body"><?php print $body ?></div>
--- templates/oap_messages.tpl.php
+++ templates/oap_messages.tpl.php
@@ -2,5 +2,5 @@
   <div id="oap-messages-tip"><?php print $tip; ?></div>
   <div id="oap-message-count"><?php print $count; ?></div>
   <div id="oap-message-notice">You have unread messages that require your attention:</div>
-  <div id="oap-message-rows"><?php print $messages; ?></div>
+  <div id="oap-message-rows"><?php print $messages_rendered; ?></div>
 </div>
