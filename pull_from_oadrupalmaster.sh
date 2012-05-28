f6647fba1ca3401c0047cd7df79a218371ef48e0
Ryan Palmer <rypalmer@gmail.com>
2012-05-28 14:34:50 -0300

    Exclude backup and wsdl dirs in pull script

--- pull_from_oadrupalmaster.sh
+++ pull_from_oadrupalmaster.sh
@@ -1,1 +1,1 @@
-rsync -rv oadrupalmaster:/var/www/html/dev.portal.oahealthcare.com/sites/all/modules/oahealthportal/ /Users/rypalmer/Sites/oahealthcare/
+rsync -rv --exclude 'backup*' --exclude 'wsdl' oadrupalmaster:/var/www/html/dev.portal.oahealthcare.com/sites/all/modules/oahealthportal/ /Users/rypalmer/Sites/oahealthcare/
