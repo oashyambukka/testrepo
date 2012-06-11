bff5ec9f4aff642109e84742c59b4f48d6b7ca39
Ryan Palmer <rypalmer@gmail.com>
2012-06-11 15:35:54 -0300

    Checklist html output

--- templates/oap_pdf_item_checklist_skill.tpl.php
+++ templates/oap_pdf_item_checklist_skill.tpl.php
@@ -1,7 +1,7 @@
-<tr>
-  <td><?php $element['#title']; ?></td>
-  <td><?php print ($element['#value'] == 1) ? '&#9632;' : '&#9633;'; ?>&nbsp;No Experience</td>
-  <td><?php print ($element['#value'] == 2) ? '&#9632;' : '&#9633;'; ?>&nbsp;Limited Experience</td>
-  <td><?php print ($element['#value'] == 3) ? '&#9632;' : '&#9633;'; ?>&nbsp;Experience</td>
-  <td><?php print ($element['#value'] == 4) ? '&#9632;' : '&#9633;'; ?>&nbsp;Highly Skilled</td>
-</tr>
+          <tr>
+            <td><?php print $element['#title']; ?></td>
+            <td><?php print ($element['#value'] == 1) ? '&#9632;' : '&#9633;'; ?>&nbsp;No Experience</td>
+            <td><?php print ($element['#value'] == 2) ? '&#9632;' : '&#9633;'; ?>&nbsp;Limited Experience</td>
+            <td><?php print ($element['#value'] == 3) ? '&#9632;' : '&#9633;'; ?>&nbsp;Experienced</td>
+            <td><?php print ($element['#value'] == 4) ? '&#9632;' : '&#9633;'; ?>&nbsp;Highly Skilled</td>
+          </tr>
