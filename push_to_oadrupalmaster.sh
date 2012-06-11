40aca2a74f8f082231eeef5d034bc6abfa6df9bd
Ryan Palmer <rypalmer@gmail.com>
2012-06-11 16:16:28 -0300

    Keep quiet

--- push_to_oadrupalmaster.sh
+++ push_to_oadrupalmaster.sh
@@ -1,3 +1,3 @@
-rsync -av --exclude '.git' /Users/rypalmer/Sites/oahealthcare/ /Users/rypalmer/Sites/dev.portal.oahealthcare.com.staging/dev.portal.oahealthcare.com/sites/all/modules/oahealthportal
-rsync -av /Users/rypalmer/Sites/oahealthcare/wsdl/oatest/ /Users/rypalmer/Sites/dev.portal.oahealthcare.com.staging/dev.portal.oahealthcare.com/sites/all/modules/oahealthportal/wsdl
-rsync -rv /Users/rypalmer/Sites/dev.portal.oahealthcare.com.staging/dev.portal.oahealthcare.com/sites/all/ oadrupalmaster:/var/www/html/dev.portal.oahealthcare.com/sites/all/
+rsync -a --exclude '.git' /Users/rypalmer/Sites/oahealthcare/ /Users/rypalmer/Sites/dev.portal.oahealthcare.com.staging/dev.portal.oahealthcare.com/sites/all/modules/oahealthportal
+rsync -a /Users/rypalmer/Sites/oahealthcare/wsdl/oatest/ /Users/rypalmer/Sites/dev.portal.oahealthcare.com.staging/dev.portal.oahealthcare.com/sites/all/modules/oahealthportal/wsdl
+rsync -r /Users/rypalmer/Sites/dev.portal.oahealthcare.com.staging/dev.portal.oahealthcare.com/sites/all/ oadrupalmaster:/var/www/html/dev.portal.oahealthcare.com/sites/all/
