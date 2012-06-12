539e94a4e20e3a9da72c387d448e87f0160ffff3
Ryan Palmer <rypalmer@gmail.com>
2012-06-11 21:26:20 -0300

    New PDF item element templates

---
+++ templates/oap_pdf_item_acknowledgement.tpl.php
@@ -0,0 +1,4 @@
+          <tr>
+            <td class="question"><?php print $element['#prefix'] . $element['#suffix']; ?>:</td>
+            <td class="answer"><?php print $element['#value']; ?></td>
+          </tr>
