52b16722755d5f476e1d3e41c8d11f6d82b5ef39
Ryan Palmer <rypalmer@gmail.com>
2012-06-13 13:33:38 -0300

    Checkboxes, other pdf item refinements

--- templates/oap_pdf_item_acknowledgement.tpl.php
+++ templates/oap_pdf_item_acknowledgement.tpl.php
@@ -1,4 +1,4 @@
           <tr>
-            <td class="question"><?php print $element['#prefix'] . $element['#suffix']; ?>:</td>
-            <td class="answer"><?php print $element['#value']; ?></td>
+            <td class="question"><?php print $element['#pdf_title']; ?>:</td>
+            <td class="answer"><?php print $element['#title']; ?></td>
           </tr>
---
+++ templates/oap_pdf_item_checkboxes.tpl.php
@@ -0,0 +1,5 @@
+          <tr>
+            <td class="question"><?php print $element['#title']; ?>:</td>
+            <td class="answer"><?php print $element['#value']; ?></td>
+          </tr>
+
