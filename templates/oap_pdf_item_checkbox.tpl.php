1f6ee01cecd72af7707b470e70460bb6a6a2a12e
Ryan Palmer <rypalmer@gmail.com>
2012-06-28 16:44:08 -0300

    Template and preprocess for PDF item checkbox

---
+++ templates/oap_pdf_item_checkbox.tpl.php
@@ -0,0 +1,4 @@
+          <tr>
+            <td class="question"><?php print $element['#pdf_title']; ?>:</td>
+            <td class="answer"><?php print $element['#title']; ?></td>
+          </tr>
